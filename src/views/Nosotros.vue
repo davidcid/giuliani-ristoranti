a29de95a2ab6ae941681849b835fe6677d26cc62
David Cid <david.cid@gmail.com>
2019-11-05 00:21:18 +0100

    started nosotros page

--- src/views/Nosotros.vue
+++ src/views/Nosotros.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="nosotros">
     <Navbar />
+    <section class="masa">
+      <img src="@/assets/working-on-pizza.jpg" alt="pizza-dough">
+      <div>
+        <p>Todas nuestras pizzas napolitanas se hornean entre 60 y 90 segundos a unos 500 grados, en un horno de piedra refractaria fabricado a mano en Nápoles por la cuarta generación de una familia de artesanos.</p>
+        <p>¿El resultado? Una pizza rica en fibra, vitaminas y nutrientes, con un aroma más intenso que respira el espíritu relajado y delicioso de la ciudad de Verona en cada uno de sus ingredientes.</p>
+      </div>
+    </section>
+    <section class="alma">
+      <img src="@/assets/pizza-chef.jpg" alt="pizza-chef">
+      <div>
+        <p>El alma de nuestras pizzas se encuentra en la harina. Las harinas que utilizamos son diferentes y alternativas, abarcando desde las tradicionales hasta las de cinco cereales. </p>
+        <p>Cada una de nuestras pizzas, elaboradas con una masa diferente, son una combinación de sabores cada vez más diversos y sorprendentes.</p>
+      </div>
+    </section>
     <Footer />
   </div>
 </template>
@@ -22,5 +36,45 @@ export default {
 <style scoped>
   .nosotros {
     margin: 0;
+    font-family: 'Fjord One', sans-serif;
   }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  section img {
+    width: 90%;
+    margin: 30px auto;
+  }
+
+  section p {
+    text-align: justify;
+    margin: 0px 30px 50px 30px;
+  }
+
+  @media only screen and (min-width: 600px) {
+    section {
+      flex-direction: row;
+      justify-content: center;
+      margin: 100px;
+    }
+
+    section img {
+      max-width: 700px;
+      height: 100%;
+      margin: 0;
+    }
+
+    section p {
+      text-align: left;
+      max-width: 300px;
+      margin: 0 30px;
+      padding-bottom: 30px;
+      font-size: 1rem;
+    }
+  }
+
 </style>
